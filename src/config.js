5dd7a15393ede6b12fb4d4ae74fde1f2fbb627e0
AleGrance <alegrance1000@gmail.com>
2025-08-11 09:04:56 -0300

    upd variables de entorno

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 import { config } from "dotenv";
 config();
 
-export const PORT_MS = process.env.PORT || 4000;
-export const DB_USER_MS = process.env.DB_USER || "prodpar";
-export const DB_PASSWORD_MS = process.env.DB_PASSWORD || "Dkp*5678$";
-export const DB_SERVER_MS = process.env.DB_SERVER || "192.168.40.40\\WINCC";
-export const DB_DATABASE_MS = process.env.DB_DATABASE || "AxFertimaxFrontera";
+export const PORT_MS = process.env.PORT;
+export const DB_USER_MS = process.env.DB_USER_MS;
+export const DB_PASSWORD_MS = process.env.DB_PASSWORD_MS;
+export const DB_SERVER_MS = process.env.DB_SERVER_MS;
+export const DB_DATABASE_MS = process.env.DB_DATABASE_MS;
